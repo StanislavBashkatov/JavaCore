f3d2bea32301cd622cad3fc418f345f7f2a073a4
StanislavAB <stas.bashckatov@yandex.ru>
2021-06-29 22:43:37 +0300

    Task_2,3

--- src/ru/geekbrains/MainLesson2.java
+++ src/ru/geekbrains/MainLesson2.java
@@ -3,16 +3,36 @@ package ru.geekbrains;
 import java.util.Arrays;
 
 public class MainLesson2 {
-    public static void main(String[] args) {
-        int[] arr = {1, 0, 0, 0, 1, 0, 1, 1, 0, 0};
-        {
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = 1 - arr[i];
 
-            }
-            System.out.println(Arrays.toString(arr));
+    public static void main(String[] args) {
+
+        //      int[] arr = {1, 0, 0, 0, 1, 0, 1, 1, 0, 0};
+        //       {
+        //          for (int i = 0; i < arr.length; i++) {
+        //               arr[i] = 1 - arr[i];
+
+        //          }
+
+
+        {
+            //   int[] arr = new int[8];
+            //  for (int i = 0; i < arr.length; i++) {
+            //     arr[i] = i * 3;
+            //  }
+
+            // System.out.println(Arrays.toString(arr));
+
+
         }
 
-    }
-}
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] *= 2;
+            }
 
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+}

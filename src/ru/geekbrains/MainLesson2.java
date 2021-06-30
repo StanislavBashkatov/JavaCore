3a2775577dfbbc7b507f7d6338e3ef7bebaf17a1
StanislavAB <stas.bashckatov@yandex.ru>
2021-06-30 21:43:27 +0300

    Task_4 Распечатывает не таблицей, почему так, пока не смог разобраться.

--- src/ru/geekbrains/MainLesson2.java
+++ src/ru/geekbrains/MainLesson2.java
@@ -4,35 +4,68 @@ import java.util.Arrays;
 
 public class MainLesson2 {
 
+
     public static void main(String[] args) {
 
-        //      int[] arr = {1, 0, 0, 0, 1, 0, 1, 1, 0, 0};
-        //       {
-        //          for (int i = 0; i < arr.length; i++) {
-        //               arr[i] = 1 - arr[i];
-
-        //          }
+        printArray(factoryArr(8));
+    }
 
 
-        {
-            //   int[] arr = new int[8];
-            //  for (int i = 0; i < arr.length; i++) {
-            //     arr[i] = i * 3;
-            //  }
+    //      int[] arr = {1, 0, 0, 0, 1, 0, 1, 1, 0, 0};
+    //       {
+    //          for (int i = 0; i < arr.length; i++) {
+    //               arr[i] = 1 - arr[i];
 
-            // System.out.println(Arrays.toString(arr));
+    //          }
 
 
-        }
+    //   int[] arr = new int[8];
+    //  for (int i = 0; i < arr.length; i++) {
+    //     arr[i] = i * 3;
+    //  }
 
-        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < 6) {
-                arr[i] *= 2;
+    // System.out.println(Arrays.toString(arr));
+
+
+    //      int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+    //    for (int i = 0; i < arr.length; i++) {
+    //        if (arr[i] < 6) {
+    //          arr[i] *= 2;
+    //      }
+
+    //   }
+    //   System.out.println(Arrays.toString(arr));
+
+    public static int[][] factoryArr(int n) {
+
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1) {
+                    arr[i][j] = 1;
+
+                }
+
             }
-
         }
-        System.out.println(Arrays.toString(arr));
+
+
+        return arr;
+    }
+
+    public static void printArray(int[][] arr) {
+        for (int[] ints : arr) {
+            for (int anInt : ints) {
+                System.out.println(anInt);
+            }
+            System.out.println();
+        }
+
     }
 
 }
+
+
+
+
+

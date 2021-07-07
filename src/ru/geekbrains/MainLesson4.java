139f91475537219367cc28b183b0bde5f0d5701b
StanislavAB <stas.bashckatov@yandex.ru>
2021-07-07 21:53:23 +0300

    X0_Game работает победа

--- src/ru/geekbrains/MainLesson4.java
+++ src/ru/geekbrains/MainLesson4.java
@@ -4,11 +4,13 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MainLesson4 {
-    static final int SIZE = 7;
+    static final int SIZE = 3;
+    static final int DOTS_TO_WIN = 3;
 
     static final char DOT_X = 'X';
     static final char DOT_0 = '0';
     static final char DOT_EMPTY = '-';
+
 
     static char[][] map;
 
@@ -19,6 +21,55 @@ public class MainLesson4 {
     public static void main(String[] args) {
         initMap();
         printMap();
+
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWinLines(DOT_X, DOTS_TO_WIN)) {
+                System.out.println("WIN");
+                break;
+            }
+            if (isFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+
+            compTurn();
+            printMap();
+            if (checkWinLines(DOT_0, DOTS_TO_WIN)) {
+                System.out.println("WIN");
+                break;
+
+            }
+
+        }
+
+    }
+
+    public static void humanTurn() {
+        int y;
+        int x;
+
+        do {
+            System.out.println("input X Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(y, x));
+        map[y][x] = DOT_X;
+
+    }
+
+
+    public static void compTurn() {
+        int y;
+        int x;
+
+        do {
+            y = random.nextInt(SIZE);
+            x = random.nextInt(SIZE);
+        } while (!isCellValid(y, x));
+        map[y][x] = DOT_0;
+
     }
 
 
@@ -35,12 +86,12 @@ public class MainLesson4 {
     public static void printMap() {
         System.out.print("  ");
         for (int i = 0; i < SIZE; i++) {
-            System.out.printf("%d ", i+1);
+            System.out.printf("%d ", i + 1);
         }
         System.out.println();
 
         for (int i = 0; i < SIZE; i++) {
-            System.out.printf("%d ", i+1);
+            System.out.printf("%d ", i + 1);
             for (int j = 0; j < SIZE; j++) {
                 System.out.printf("%c ", map[i][j]);
             }
@@ -48,6 +99,60 @@ public class MainLesson4 {
 
         }
 
+    }
+
+
+    public static boolean isCellValid(int y, int x) {
+        if (y < 0 || x < 0 || y >= SIZE || x >= SIZE) {
+            return false;
+        }
+
+        return map[y][x] == DOT_EMPTY;
+
+    }
+
+    public static boolean isFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) {
+                    return false;
+                }
+
+            }
+        }
+        return true;
+    }
+
+    static boolean checkLine(int cy, int cx, int vy, int vx, char dot, int dotsToWin) {
+        if (cx + vx * (dotsToWin) + 1 > SIZE - 1 || cy + vy * (dotsToWin - 1) > SIZE - 1 ||
+                cy + vy * (dotsToWin - 1) < 0) {
+            return false;
+        }
+
+        for (int i = 0; i < dotsToWin; i++) {
+            if (map[cy + i * vy][cx + i * vx] != dot) {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+    static boolean checkWinLines(char dot, int dotsToWin) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (checkLine(i, j, 0, 1, dot, dotsToWin) ||
+                        checkLine(i, j, 1, 0, dot, dotsToWin) ||
+                        checkLine(i, j, 1, 1, dot, dotsToWin) ||
+                        checkLine(i, j, -1, 1, dot, dotsToWin)) {
+                    return true;
+                }
+
+            }
+
+        }
+        return false;
     }
 
 }

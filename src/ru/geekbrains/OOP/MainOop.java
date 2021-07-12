56c6b652ec7f29ae7462107638878b8f58c0e717
StanislavAB <stas.bashckatov@yandex.ru>
2021-07-12 21:57:02 +0300

    OOP

--- src/ru/geekbrains/OOP/MainOop.java
+++ src/ru/geekbrains/OOP/MainOop.java
@@ -3,12 +3,11 @@ package ru.geekbrains.OOP;
 public class MainOop {
     public static void main(String[] args) {
         Person[] persons = new Person[5];
-        persons[0] = new Person("Ivanov Ivan", "courier", "ivanov@person.com", 7775455, 30000, 20);
-        persons[1] = new Person("Petrov Vadim", "designer", "petrov@person.com", 7775456, 100000, 31);
-        persons[2] = new Person("Sidorov Andew", "developer", "sidorov@person.com", 7775457, 300000, 42);
-        persons[3] = new Person("Nikolaev Oleg", "project manager", "nikolaev@person.com", 7775458, 150000, 47);
-        persons[4] = new Person("Koshkin Makar", "clerk", "koshkin@person.com", 7775459, 27000, 21);
-
+        persons[0] = new Person("Anton Antonov", "courier", "ivanov@person.com", 7775455, 30000, 20);
+        persons[1] = new Person("Nickolay Bolshov", "designer", "petrov@person.com", 7775456, 100000, 31);
+        persons[2] = new Person("Dmitiy Medvedev", "developer", "sidorov@person.com", 7775457, 300000, 42);
+        persons[3] = new Person("Saveliy Kramorov", "project manager", "nikolaev@person.com", 7775458, 150000, 47);
+        persons[4] = new Person("Anna Ivanova", "clerk", "anna@person.com", 7773359, 89000, 54);
 
         for (int i = 0; i < persons.length; i++) {
             if (persons[i].getAge() > 40) {
